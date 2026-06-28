6114d8ff5833391c20454a9bbf37f5712f0747ef
Justin Tomala <justintomala01@gmail.com>
2026-06-28 18:30:01 -0500

    Actualizacion de timer_task

--- app/async_tasks/timer_task.py
+++ app/async_tasks/timer_task.py
@@ -1,22 +1,17 @@
 import asyncio
+
 from app.state.store import state
-from app.state.reducer import update
+
 
 async def timer_task():
 
-    global state
-
     while True:
+
         await asyncio.sleep(1)
 
-        new_timer = state.timer - 1
+        if state.timer > 0:
+            state.timer -= 1
+        else:
+            state.timer = 30
 
-        if new_timer <= 0:
-            new_timer = 30
-
-        state = update(state, {
-            "type": "UPDATE_TIMER",
-            "timer": new_timer
-        })
-
-        print("Timer actualizado:", state.timer)
+        print(f"⏳ Timer: {state.timer}")
