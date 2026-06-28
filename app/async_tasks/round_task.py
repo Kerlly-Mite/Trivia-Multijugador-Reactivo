67a9ef6c868a07280146f1ef16f73979fb722c69
Justin Tomala <justintomala01@gmail.com>
2026-06-28 18:30:50 -0500

    Actualizacion de round_task

--- app/async_tasks/round_task.py
+++ app/async_tasks/round_task.py
@@ -1,16 +1,14 @@
 import asyncio
+
 from app.state.store import state
-from app.state.reducer import update
+
 
 async def round_task():
 
-    global state
-
     while True:
+
         await asyncio.sleep(30)
 
-        state = update(state, {
-            "type": "NEXT_ROUND"
-        })
+        state.round_number += 1
 
-        print("Nueva ronda:", state.round_number)
+        print(f"🎮 Nueva ronda: {state.round_number}")
