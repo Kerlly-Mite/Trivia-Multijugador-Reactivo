262e4fe49fb5b6c88ac21b612e0fc282bd094153
Kerlly Mite <belinda115uwu@gmail.com>
2026-06-28 18:05:16 -0500

    Mejora del main

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from reactpy import run
 from app.components.layout import Layout
 from app.components.game import Game
 
-from app.state.tasks.timer_task import timer_task
-from app.state.tasks.round_task import round_task
-from app.state.tasks.events_task import events_task
+from app.async_tasks.timer_task import timer_task
+from app.async_tasks.round_task import round_task
+from app.async_tasks.events_task import events_task
 
 
 async def main():
 
-    # correr tareas en paralelo (OBLIGATORIO RUBRICA)
+    # correr tareas en paralelo 
     asyncio.create_task(timer_task())
     asyncio.create_task(round_task())
     asyncio.create_task(events_task())
