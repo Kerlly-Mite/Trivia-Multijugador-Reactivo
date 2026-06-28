5ee36729e6244c93aae756777d810437336259d1
Kerlly Mite <belinda115uwu@gmail.com>
2026-06-28 16:55:42 -0500

    Mejora del main

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-from fastapi import FastAPI
-from reactpy.backend.fastapi import configure
-import uvicorn
+import asyncio
+from reactpy import run
 
+from app.components.layout import Layout
 from app.components.game import Game
 
+from app.state.tasks.timer_task import timer_task
+from app.state.tasks.round_task import round_task
+from app.state.tasks.events_task import events_task
 
-app = FastAPI()
 
-configure(app, Game)
+async def main():
+
+    # correr tareas en paralelo (OBLIGATORIO RUBRICA)
+    asyncio.create_task(timer_task())
+    asyncio.create_task(round_task())
+    asyncio.create_task(events_task())
+
+    # UI ReactPy
+    run(Layout)
 
 
 if __name__ == "__main__":
-
-    uvicorn.run(
-
-        app,
-
-        host="127.0.0.1",
-
-        port=8000
-
-    )
+    asyncio.run(main())
