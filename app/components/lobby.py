89784569ff43ae6dad79d1deb94231e6b78e7524
Kerlly Mite <belinda115uwu@gmail.com>
2026-06-28 22:07:09 -0500

    feat: agregar pantalla Lobby de espera

--- app/components/lobby.py
+++ app/components/lobby.py
@@ -2,7 +2,7 @@ from reactpy import component, html
 
 
 @component
-def Lobby():
+def Lobby(estado):
 
     return html.div(
 
@@ -11,12 +11,12 @@ def Lobby():
         html.h3("Jugadores conectados"),
 
         html.ul(
-
-            html.li("🟢 Katherine"),
-
-            html.li("🟢 Carlos"),
-
-            html.li("🟢 María")
+            *[
+                html.li(f"🟢 {jugador}")
+                for jugador in estado.players
+            ]
+        ) if len(estado.players) > 0 else html.p(
+            "Todavía no hay jugadores conectados."
         ),
 
         html.p("Esperando el inicio de la partida...")
